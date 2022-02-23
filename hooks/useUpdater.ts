99c6e830e8939d0c45e025fd5033a65709d76548
Eick <oe52920@gmail.com>
2022-02-23 11:28:36 +0800

    chore: 删除多余文件

--- hooks/useUpdater.ts
+++
@@ -1,7 +0,0 @@
-import { useState, useCallback } from "react";
-
-export function useUpdater() {
-  const [, s] = useState(0);
-  const updater = useCallback(() => s((v) => v + 1), []);
-  return updater;
-}
